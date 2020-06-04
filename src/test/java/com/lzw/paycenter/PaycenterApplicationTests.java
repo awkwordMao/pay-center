df001957d58878fd520b8ae1450ae6fa4802c8bd
awkwordMao <2624824150@qq.com>
2020-06-04 08:59:13 +0800

    Initial commit

---
+++ src/test/java/com/lzw/paycenter/PaycenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzw.paycenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaycenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
